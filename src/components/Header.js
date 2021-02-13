d0081b188ec3fdaaa08d2a0f5e83651d1109db0b
chen <23054115+cwkang1998@users.noreply.github.com>
2021-02-14 00:59:11 +0800

    feat: 'Upgrade' the colour scheme and navigation styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,55 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 import config from "../../data/SiteConfig";
 import styles from "./Header.module.scss";
 
-const Header = () => (
-  <header>
-    <h1>
-      <Link to="/" activeClassName={styles.activeNav}>
-        {config.siteTitle}
-      </Link>
-    </h1>
-    <nav>
-      <ul className={styles.mainNav}>
-        <li>
-          <Link to="/blog" activeClassName={styles.activeNav}>
-            Blog
-          </Link>
-        </li>
-        <li>
-          <Link to="/portfolio" activeClassName={styles.activeNav}>
-            Portfolio
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" activeClassName={styles.activeNav}>
-            Contact
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" activeClassName={styles.activeNav}>
-            About
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "images/icon.png" }) {
+        childImageSharp {
+          fixed(width: 75, height: 75) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <header>
+      <h1>
+        <Link to="/" activeClassName={styles.activeNav}>
+          {/* {config.siteTitle} */}
+          <Img fixed={data.file.childImageSharp.fixed} />
+        </Link>
+      </h1>
+      <nav>
+        <ul className={styles.mainNav}>
+          <li>
+            <Link to="/blog" activeClassName={styles.activeNav}>
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link to="/portfolio" activeClassName={styles.activeNav}>
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" activeClassName={styles.activeNav}>
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" activeClassName={styles.activeNav}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
