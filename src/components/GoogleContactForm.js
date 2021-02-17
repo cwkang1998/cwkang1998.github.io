155bce99bba28cf3b318f60891a01123fda8985e
chen <23054115+cwkang1998@users.noreply.github.com>
2021-02-18 02:52:44 +0800

    feat: Complete contact page with form using google form

---
+++ src/components/GoogleContactForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./GoogleContactForm.module.scss";
+
+const GoogleContactForm = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  return (
+    <div>
+      <form
+        method="GET"
+        action="https://docs.google.com/forms/d/e/1FAIpQLScKebTypPR5iOz1uBlrrJwn3sdt1Ma0LcnIDxrsRPU94SzdWQ/formResponse"
+        encType="text/plain"
+        target="gFormSubmit"
+        onSubmit={() => setIsSubmitted(true)}
+        hidden={isSubmitted}
+      >
+        <label htmlFor="entry.1645378407">Name</label>
+        <input
+          name="entry.1645378407"
+          type="text"
+          placeholder="John Doe"
+          required
+        />
+        <label htmlFor="entry.1553054032">Email</label>
+        <input
+          name="entry.1553054032"
+          type="email"
+          placeholder="youemail@someemail.com"
+          required
+        />
+        <label htmlFor="entry.2007973957">Message</label>
+        <textarea
+          name="entry.2007973957"
+          rows="5"
+          placeholder="I would like an appointment."
+          required
+        />
+        <input type="submit" value="Submit" />
+        <iframe name="gFormSubmit" title="gFormSubmit" hidden />
+      </form>
+      <h3 hidden={!isSubmitted}>Thank you! I will contact you soon.</h3>
+    </div>
+  );
+};
+
+export default GoogleContactForm;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
 import Layout from "../layout";
+import GoogleContactForm from "../components/GoogleContactForm";
 import config from "../../data/SiteConfig";
 
 const ContactPage = () => (
@@ -8,6 +9,7 @@ const ContactPage = () => (
     <main>
       <Helmet title={`Contact | ${config.siteTitle}`} />
       <h1>Contact</h1>
+      <GoogleContactForm />
     </main>
   </Layout>
 );
